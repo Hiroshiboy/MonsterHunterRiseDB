c8b384be6e5e1fdd1c1f7b01c5ef36703f479ad6
Neil Sano <neil.hiroshi.sano@gmail.com>
2021-10-22 17:26:28 -0400

    Added gitignore file for node_modules and added an appLoad.js file to load all monsters from a json.

---
+++ appLoadData.js
@@ -0,0 +1,37 @@
+var AWS = require("aws-sdk");
+var fs = require("fs");
+
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+console.log("Importing Monsters into the DB. Just a second...");
+
+var allMonsters = JSON.parse(fs.readFileSync('monsters.json', 'utf8'));
+allMonsters.forEach((monster) => {
+    var params = {
+        TableName: "MonsterHunterRiseDB",
+        Item: {
+            "monster_id" : monster.monster_id,
+            "monster_type" : monster.monster_type,
+            "monster_name": monster.monster_name,
+            "threat_level" : monster.threat_level,
+            "elements" : monster.elements,
+            "ailments" : monster.ailments,
+            "weaknesses" : monster.weaknesses,
+            "resistances" : monster.resistances,
+            "habitats" : monster.habitats,
+        }
+    };
+    docClient.put(params, (err, data) => {
+        if(err){
+            console.error("Unable to Add Monster", monster.monster_name, ". Error JSON: ", JSON.stringify(err, null, 2));
+        } else {
+            console.log(`${monster.monster_name} Inserted`);
+        }
+    });
+
+});
