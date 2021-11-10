5af191d5b7f8b797a18043a2c3449a4a081b91e6
Neil Sano <neil.hiroshi.sano@gmail.com>
2021-11-10 16:02:23 -0500

    Move Deployed Lambda Functions to a new folder

---
+++ LambdaFunctions/lambdaFunction_query.js
@@ -0,0 +1,39 @@
+var AWS = require("aws-sdk");
+
+var dynamodb = new AWS.DynamoDB({
+  region: "us-east-2",
+  apiVersion: "2012-08-10",
+});
+
+exports.handler = (event, context, cb) => {
+  const params = {
+    TableName: "MonsterHunterRiseDB",
+    IndexName: "monster_type-index",
+    KeyConditionExpression: "monster_type = :mt",
+    ExpressionAttributeValues: {
+      ":mt": { S: event.queryStringParameters.type},
+    },
+  };
+  dynamodb
+    .query(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        cb(err);
+      } else {
+        const unmarshalledData = data.Items.map((el) => {
+          return AWS.DynamoDB.Converter.unmarshall(el);
+        });
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(unmarshalledData),
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+          },
+        };
+
+        console.log(response);
+        cb(null, response);
+      }
+    });
+};
